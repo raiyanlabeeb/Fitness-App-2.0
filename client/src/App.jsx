9927e873563898b54b605c5e144d2c16e59cbe4e
raiyanlabeeb <raiyan.labeeb@gmail.com>
2025-06-09 19:26:31 -0400

    added git ignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,34 @@ import { useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import TestMessage from "./components/testMessage";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
+
+function HomePage() {
+  return <h2>Welcome to the Home Page</h2>;
+}
+
+function LoginPage() {
+  return <h2>Please Log In</h2>;
+}
 function App() {
 
   return (
-    <div>
-      <h1>Message from backend:</h1>
-      <TestMessage />
-    </div>
+    // <div>
+    //   <h1>Message from backend:</h1>
+    //   <TestMessage />
+    // </div>
+
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/login">Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
